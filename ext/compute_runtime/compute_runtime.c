e6fb055e025e1f70f0b2f0779fbb769caab74c78
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-07 13:53:20 +0000

    WIP

--- ext/compute_runtime/compute_runtime.c
+++ ext/compute_runtime/compute_runtime.c
@@ -53,31 +53,31 @@ fastly_status_t fastly_object_store_insert(object_store_handle_t object_store_ha
 WASM_IMPORT("fastly_geo", "lookup")
 int fastly_geo_lookup(const char *ip, size_t ip_len, char *value, size_t value_max_len, size_t* nwritten);
 
+static void _raise_fastly_error(fastly_status_t status, int line, const char *func) {
+  rb_raise(rb_eRuntimeError, "Fastly error: %d at %s:%d", status, func, line);
+}
+
+#define HANDLE_FASTLY_ERROR(status) if (status != 0) _raise_fastly_error(status, __LINE__, __func__);
+
 static VALUE _fastly_http_body_new(VALUE obj)
 {
   body_handle_t body_handle;
   fastly_status_t status = fastly_http_body_new(&body_handle);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_body_new failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return INT2NUM(body_handle);
 }
 
 static VALUE _fastly_http_body_append(VALUE obj, VALUE dest, VALUE src)
 {
   fastly_status_t status = fastly_http_body_append(NUM2INT(dest), NUM2INT(src));
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_body_append failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return Qnil;
 }
 
 static VALUE _fastly_http_body_close(VALUE obj, VALUE handle)
 {
   fastly_status_t status = fastly_http_body_close(NUM2INT(handle));
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_body_close failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return Qnil;
 }
 
@@ -89,9 +89,7 @@ static VALUE _fastly_http_body_write(VALUE obj, VALUE body_handle, VALUE buf, VA
   body_write_end_t write_end = NUM2INT(end);
   size_t nwritten;
   fastly_status_t status = fastly_http_body_write(handle, buf_ptr, buf_len, write_end, &nwritten);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_body_write failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return INT2NUM(nwritten);
 }
 
@@ -99,12 +97,12 @@ static VALUE _fastly_http_body_read(VALUE obj, VALUE handle, VALUE data, VALUE d
 {
   body_handle_t body_handle = NUM2INT(handle);
   uint8_t* data_ptr = (uint8_t*)RSTRING_PTR(data);
+  rb_str_modify(data);
   size_t data_max_len_val = NUM2INT(data_max_len);
   size_t nwritten;
   fastly_status_t status = fastly_http_body_read(body_handle, data_ptr, data_max_len_val, &nwritten);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_body_read failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
+  rb_str_set_len(data, nwritten);
   return INT2NUM(nwritten);
 }
 
@@ -113,18 +111,14 @@ static VALUE _fastly_http_req_downstream_client_ip_addr(VALUE obj)
   uint8_t octets[16];
   size_t nwritten;
   int status = fastly_http_req_downstream_client_ip_addr(octets, &nwritten);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_req_downstream_client_ip_addr failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return rb_str_new((const char*)octets, nwritten);
 }
 
 static VALUE _fastly_http_resp_new(VALUE obj) {
   response_handle_t resp_handle;
   fastly_status_t status = fastly_http_resp_new(&resp_handle);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_resp_new failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return INT2NUM(resp_handle);
 }
 
@@ -133,9 +127,7 @@ static VALUE _fastly_http_resp_send_downstream(VALUE obj, VALUE resp_handle, VAL
   body_handle_t body_handle_handle = NUM2INT(body_handle);
   int32_t is_streaming = NUM2INT(streaming);
   fastly_status_t status = fastly_http_resp_send_downstream(handle, body_handle_handle, is_streaming);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_http_resp_send_downstream failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return Qnil;
 }
 
@@ -144,9 +136,7 @@ static VALUE _fastly_object_store_open(VALUE obj, VALUE name) {
   const char *name_ptr = RSTRING_PTR(name);
   size_t name_len = RSTRING_LEN(name);
   fastly_status_t status = fastly_object_store_open(name_ptr, name_len, &object_store_handle);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_object_store_open failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return INT2NUM(object_store_handle);
 }
 
@@ -156,9 +146,7 @@ static VALUE _fastly_object_store_lookup(VALUE obj, VALUE object_store_handle, V
   size_t key_len = RSTRING_LEN(key);
   body_handle_t body_handle;
   fastly_status_t status = fastly_object_store_lookup(handle, key_ptr, key_len, &body_handle);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_object_store_lookup failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return INT2NUM(body_handle);
 }
 
@@ -168,22 +156,19 @@ static VALUE _fastly_object_store_insert(VALUE obj, VALUE object_store_handle, V
   size_t key_len = RSTRING_LEN(key);
   body_handle_t body_handle_handle = NUM2INT(body_handle);
   fastly_status_t status = fastly_object_store_insert(handle, key_ptr, key_len, body_handle_handle);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_object_store_insert failed");
-  }
+  HANDLE_FASTLY_ERROR(status);
   return Qnil;
 }
 
-static VALUE _fastly_geo_lookup(VALUE obj, VALUE ip, VALUE value) {
+static VALUE _fastly_geo_lookup(VALUE obj, VALUE ip, VALUE value, VALUE value_max_len) {
   const char *ip_ptr = RSTRING_PTR(ip);
   size_t ip_len = RSTRING_LEN(ip);
   char *value_ptr = RSTRING_PTR(value);
-  size_t value_len = RSTRING_LEN(value);
+  rb_str_modify(value);
   size_t nwritten;
-  fastly_status_t status = fastly_geo_lookup(ip_ptr, ip_len, value_ptr, value_len, &nwritten);
-  if (status != 0) {
-    rb_raise(rb_eRuntimeError, "fastly_geo_lookup failed");
-  }
+  fastly_status_t status = fastly_geo_lookup(ip_ptr, ip_len, value_ptr, value_max_len, &nwritten);
+  HANDLE_FASTLY_ERROR(status);
+  rb_str_set_len(value, nwritten);
   return INT2NUM(nwritten);
 }
 
@@ -206,5 +191,5 @@ Init_compute_runtime(void)
   rb_define_module_function(rb_mABI, "fastly_object_store_open", _fastly_object_store_open, 1);
   rb_define_module_function(rb_mABI, "fastly_object_store_lookup", _fastly_object_store_lookup, 2);
   rb_define_module_function(rb_mABI, "fastly_object_store_insert", _fastly_object_store_insert, 3);
-  rb_define_module_function(rb_mABI, "fastly_geo_lookup", _fastly_geo_lookup, 2);
+  rb_define_module_function(rb_mABI, "fastly_geo_lookup", _fastly_geo_lookup, 3);
 }
